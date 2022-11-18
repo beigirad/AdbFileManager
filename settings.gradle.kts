be79f1f6784e96973eb20d87e0decb1e0e0ca647
Farhad Beigirad <f.beigirad@gmail.com>
2022-11-18 21:31:46 +0330

    initial project

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    }
+    
+}
+rootProject.name = "AdbFileExplorer"
+
