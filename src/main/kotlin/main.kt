66e0f21727c76b4464d7d23e0df0a89f929fe757
Farhad Beigirad <f.beigirad@gmail.com>
2022-11-21 00:43:36 +0330

    add popup menu with delete and duplicate options

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,16 +13,20 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.CopyAll
+import androidx.compose.material.icons.twotone.Delete
 import androidx.compose.material.icons.twotone.Folder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -29,11 +34,17 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.isSecondaryPressed
+import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberCursorPositionProvider
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import se.vidstige.jadb.JadbConnection
 import se.vidstige.jadb.JadbDevice
@@ -43,6 +54,8 @@ import se.vidstige.jadb.RemoteFile
 @Composable
 fun App() {
     Row(modifier = Modifier.horizontalScroll(rememberScrollState())) {
+        val coroutines = rememberCoroutineScope { Dispatchers.IO }
+
         var selectedDevice by remember { mutableStateOf<JadbDevice?>(null) }
         var devices by remember { mutableStateOf(emptyList<JadbDevice>()) }
 
@@ -78,11 +91,12 @@ fun App() {
         )
         fullPath.forEachIndexed { index, pathSection ->
             var folders by remember { mutableStateOf(listOf<RemoteFile>()) }
+            var version by remember { mutableStateOf(0) }
             val path =
                 if (fullPath.size == 1) "/"
                 else fullPath.drop(1).take(index).joinToString(prefix = "/", separator = "/")
 
-            LaunchedEffect(pathSection, selectedDevice) {
+            LaunchedEffect(pathSection, selectedDevice, version) {
                 withContext(Dispatchers.IO) {
                     folders =
                         selectedDevice?.list(path)
@@ -112,6 +126,32 @@ fun App() {
                         isSelected = fullPath.take(index + 1).plus(file.path) == fullPath.take(index + 2),
                         onSelect = {
                             fullPath = fullPath.take(index + 1).plus(file.path)
+                        },
+                        onDelete = {
+                            coroutines.launch {
+                                val stream = if (file.isDirectory)
+                                    selectedDevice!!.executeShell("rm", "-r", "$path/${file.path}")
+                                else
+                                    selectedDevice!!.executeShell("rm", "$path/${file.path}")
+                                val message = stream.readBytes().decodeToString()
+                                if (message.isNotBlank())
+                                    println(message)
+                                version++
+                            }
+                        },
+                        onDuplicate = {
+                            coroutines.launch {
+                                val stream =
+                                    selectedDevice!!.executeShell(
+                                        "cp",
+                                        "$path/${file.path}",
+                                        "$path/${file.path}_2"
+                                    )
+                                val message = stream.readBytes().decodeToString()
+                                if (message.isNotBlank())
+                                    println(message)
+                                version++
+                            }
                         })
                 }
             }
@@ -132,7 +172,47 @@ private fun FileItem(
     file: RemoteFile,
     isSelected: Boolean,
     onSelect: () -> Unit,
+    onDelete: () -> Unit,
+    onDuplicate: () -> Unit,
 ) {
+    var showPopUp by remember { mutableStateOf(false) }
+
+    if (showPopUp) {
+        Popup(
+            onDismissRequest = { showPopUp = false },
+            focusable = true,
+            popupPositionProvider = rememberCursorPositionProvider()
+        ) {
+            Card {
+                Column(
+                    modifier = Modifier
+                        .width(150.dp)
+                        .padding(4.dp)
+                ) {
+                    PopupItem(
+                        onClick = {
+                            onDelete()
+                            showPopUp = false
+                        },
+                        content = {
+                            Icon(imageVector = Icons.TwoTone.Delete, contentDescription = null)
+                            Text(text = "Delete")
+                        })
+
+                    PopupItem(
+                        onClick = {
+                            onDuplicate()
+                            showPopUp = false
+                        },
+                        content = {
+                            Icon(imageVector = Icons.TwoTone.CopyAll, contentDescription = null)
+                            Text(text = "Duplicate")
+                        })
+                }
+            }
+        }
+    }
+
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
@@ -141,6 +221,12 @@ private fun FileItem(
             .background(if (isSelected) Color.LightGray else Color.Transparent)
             .clickable {
                 onSelect()
+            }
+            .onPointerEvent(PointerEventType.Press) {
+                if (it.buttons.isSecondaryPressed) {
+                    onSelect()
+                    showPopUp = true
+                }
             }
             .padding(4.dp)
     ) {
@@ -153,6 +239,22 @@ private fun FileItem(
             overflow = TextOverflow.Ellipsis
         )
     }
+}
+
+@Composable
+private fun PopupItem(
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.small)
+            .clickable(onClick = onClick)
+            .padding(4.dp),
+        content = content
+    )
 }
 
 fun main() = application {
